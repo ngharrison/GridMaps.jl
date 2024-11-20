47d8f6f40e6747c35b9005fe7c3f1a5084a3710a
Documenter.jl <documenter@juliadocs.github.io>
2024-11-20 21:11:25 +0000

    build based on 3d3890a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GridMaps","category":"page"},{"location":"#GridMaps","page":"Home","title":"GridMaps","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GridMaps.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GridMaps]","category":"page"}]
+}
